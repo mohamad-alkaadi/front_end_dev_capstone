eef1026dfbcd6c72433fb8c906c84442e1598847
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-09-12 04:32:15 +0300

    links

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div>
       <NavBar/>
       <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='' element={<Home/>}/>
           <Route path='/front_end_dev_capstone/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/menu' element={<Menu/>}/>
